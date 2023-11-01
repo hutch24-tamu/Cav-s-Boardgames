aa397fb35c18003fa7dfd8c3f0665dcce6a16ecc
sushicatalex <asfgonce@gmail.com>
2023-11-01 11:10:09 -0500

    started main py file with pseudo-code in it

---
+++ main.py
@@ -0,0 +1,22 @@
+# Pseudo code/Planning
+
+# Get user input of games they’ve played, ask for at least 3 maybe
+# while they want to enter more data, let them enter
+#   name of board game
+#   Their group rating of the board game
+#       int input range bound from 1-10 
+
+# Black Box Recommending
+# gonna recommend games with similar factors for games user rating highly
+# factors: 
+#   family of game 
+#   player count - ‘minplayers’ & ‘maxplayers’ in xml
+#   game length - ‘minplaytime’ & ‘maxplaytime’ in xml
+#   complexity - ‘averageweight’ in xml
+#   age rating - ‘age’ in xml
+#   designer/publisher of games
+#   year of game release
+
+# output top 10 games we’d think they like
+#   pretty print a list
+#   let user let us know if they’ve played any of them and their ratings?
