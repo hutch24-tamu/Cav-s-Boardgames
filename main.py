1c16ae65232679721d3145bd461c98bd396138b5
sushicatalex <asfgonce@gmail.com>
2023-11-16 10:04:56 -0600

    added code to searchForGames function, not complete though

--- main.py
+++ main.py
@@ -9,12 +9,27 @@
 def searchForGame(gameTitle):
     # call the api search function on gameTitle
     # get a list of game id's returned probably
+    searchedGames = [] #change for api call instead
     # loop through those games 
     #   add to possibleGames list if not an expansion
     #   ignore otherwise
+    possibleGames = []
+    for game in searchedGames:
+        boardgameCategoryID = "0000" #replace with api call for boardgamecategory id number
+        # may need to change this to a loop through the IDs because 
+        # boardgamecategory can be listed multiple times and we want to make sure none of them
+        # are 1024 for expansion
+        # may don't need loop and could just do if "1024" in boardgameCategoryIDs, maybe
+        if boardgameCategoryID == "1024":
+            possibleGames.append(game)
+
     # display possibleGames to user as a numbered list format
+    for i, game in enumerate(possibleGames):
+        print(f"{i+1}: {game}")
     # take user input - 1 bc zero index
+    userSelection = int(input("Select with game you meant by inputing it's number")) - 1
     # return game selected by user
+    return possibleGames[userSelection]
 
 # Black Box Recommending
 # gonna recommend games with similar factors for games user rating highly
