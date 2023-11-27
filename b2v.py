49172ee12577bf17656a9f5384192ec0d66da2e1
hutch24-tamu <hutch24@tamu.edu>
2023-11-27 00:06:52 -0600

    Pretty much completed the board2vec and sortCosSim functions, should be double checked

--- b2v.py
+++ b2v.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests as r
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
 
 
 def board2vec(gameID):
@@ -26,19 +27,62 @@ def board2vec(gameID):
         returns a tuple where index 0 is the gameID and index 1 is the vectorized version of the game
     """
     game = BeautifulSoup(r.get(f'https://boardgamegeek.com/xmlapi/boardgame/{gameID}').content, 'xml')
-    # example for how to access data from searchForGames
-    # boardgameCategories = game.find_all('boardgamecategory')
-    #     isExpansion = False
-    #     for category in boardgameCategories:
-    #         if category['objectid'] == '1042':
-    #             # print("expansion!")
-    #             isExpansion = True
+    boardgameCategories = game.find_all('boardgamecategory')
+    isExpansion = False
+    for category in boardgameCategories:
+        if category['objectid'] == '1042':
+            isExpansion = True
 
-    # player counts
-    minPlayers = int(game.find('minplayers').text)
-    print(minPlayers, type(minPlayers))
-    # maxPlayers = game.find('maxplayers')
-def sortCosSim(boardgamelist, usergames):
+    # Return empty vector, indicating that something is wrong (expansion pack, not actual board game)
+    # might not be needed if we already do this validation elsewehere
+    if isExpansion:
+        return (gameID, [])
+
+    # Get information from the xml. All are in try-except because some have '' returned when trying 
+    # to access the tree, so int('') creates an error
+    try:
+        minPlayers = int(game.find('minplayers').text)
+    except:
+        minPlayers = 0
+    try:
+        maxPlayers = int(game.find('maxplayers').text)
+    except:
+        maxPlayers = 0
+    try:
+        minPlayTime = int(game.find('minplaytime').text)
+    except:
+        minPlayTime = 0
+    try:
+        maxPlayTime = int(game.find('maxplaytime').text)
+    except:
+        maxPlayTime = 0
+    try:
+        childrensGameRank = float(game.find('childrensgame_rank'))
+    except:
+        childrensGameRank = 0
+    try:
+        familyGameRank = float(game.find('familygame_rank'))
+    except:
+        familyGameRank = 0
+    try:
+        averageWeight = float(game.find('averageweight'))
+    except:
+        averageWeight = 0
+    try:
+        age = int(game.find('age').text)
+    except:
+        age = 0
+    try:
+        yearPublished = int(game.find('yearpublished').text)
+    except:
+        yearPublished = 0
+
+    vec = [minPlayers, maxPlayers, minPlayTime, maxPlayTime, childrensGameRank, familyGameRank, averageWeight, age, yearPublished]
+
+    return (gameID, vec)
+
+
+def sortCosSim(boardgamelist, usergames, n=10):
     """
     
     Description:
@@ -55,9 +99,54 @@ def sortCosSim(boardgamelist, usergames):
     
         
     """
-    pass
+    # Only get the vectors, and normalize them
+    gameList = []
+    for game in boardgamelist:
+        gameList.append(normalize([game[1]])[0])
+
+    # Only get the vectors, and normalize them
+    userGamesList = []
+    for game in usergames:
+        userGamesList.append(normalize([game[1]])[0])
+
+    
+    # [i][j] index in cos sim matrix is a dot product of the ith column, jth row
+    dotProducts = cosine_similarity(userGamesList, gameList)
+
+    toReturn = []
+
+    for i in range(len(userGamesList)):
+        mostSimilar = []
+        cosineScores = dotProducts[i]
+        for j in range(len(boardgamelist)):
+            mostSimilar.append((boardgamelist[j][0], cosineScores[j]))
+
+        mostSimilar.sort(key=lambda x: x[1], reverse=True)
+        toReturn.append(mostSimilar[:n])
+
+    # TODO: Need to return the correct type. Thus far returns ('<id>', cosine)
+    return toReturn
+        
 
 def main():
-    board2vec('35424')
+    # Used to test my code
+    # au = board2vec('14978')
+    # bu = board2vec('12354')
+    # cu = board2vec('2586')
+    # du = board2vec('9831')
+    # eu = board2vec('5677')
+    # userGamesList = [au, bu, cu, du, eu]
+    # a = board2vec('164928')
+    # b = board2vec('12358')
+    # c = board2vec('7895')
+    # d = board2vec('78954')
+    # e = board2vec('85321')
+    # gameList = [a, b, c, d, e]
+    # sortCosSim(gameList, userGamesList)
+
+    pass
+    
+
+    
 if __name__ == "__main__":
     main()
